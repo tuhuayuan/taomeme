49b97bde4b29ce513a6fbd5e6bc3e8c5a78f2ec4
tuhuayuan <tuhuayuan@gmail.com>
2013-02-06 13:10:11 +0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Taomeme::Application.config.secret_token = 'af2b6b066c5d41f4d0f9ecb12801f8f0ee99cdd3aaf660f89b12b7cc4eba00b53861108881d94365cc8bb71935091d889a3880a75268e6d10a88012cca309ed5'
